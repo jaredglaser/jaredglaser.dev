369a2df611f4eba28e850a83da0c8f878f0504a7
Jared Glaser <jared@glaserfamily.net>
2026-08-21 21:39:05 -0400

    start customizing

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,14 +2,14 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
-    title: "AstroPaper",
-    description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    url: "https://jaredglaser.dev/",
+    title: "jaredglaser.dev",
+    description: "A dev blog made using Astropaper",
+    author: "Jared Glaser",
+    profile: "https://jaredglaser.dev",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "America/New_York",
     dir: "ltr",
   },
   posts: {
@@ -29,10 +29,9 @@ export default defineAstroPaperConfig({
     search: "pagefind",
   },
   socials: [
-    { name: "github",   url: "https://github.com/satnaing/astro-paper" },
-    { name: "x",        url: "https://x.com/username" },
-    { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
-    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+    { name: "github",   url: "https://github.com/jaredglaser" },
+    { name: "linkedin", url: "https://www.linkedin.com/in/jared-glaser" },
+    //{ name: "mail",     url: "mailto:yourmail@gmail.com" },
   ],
   shareLinks: [
     { name: "whatsapp", url: "https://wa.me/?text=" },
--- src/content/pages/about.md
+++ src/content/pages/about.md
@@ -5,7 +5,7 @@ description: "A bit about me and this blog."
 
 AstroPaper is a minimal, accessible and SEO-friendly blog theme built with [Astro](https://astro.build/) and [Tailwind CSS](https://tailwindcss.com/).
 
-![Astro Paper](@/assets/images/astropaper-og.jpg)
+![Jared and Cosmo](@/assets/images/jared-and-cosmo.jpg)
 
 AstroPaper provides a solid foundation for blogs, or even portfolios\_ with full markdown support, built-in dark mode, and a clean layout that works out-of-the-box.
 
